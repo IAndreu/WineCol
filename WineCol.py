faf3cbdd482a5f4277c39ee2b2915846a6d43955
Ignasi Andreu <33428731+IAndreu@users.noreply.github.com>
2021-12-06 17:22:33 +0100

    Create WineCol.py
    The first version of the tool. Takes only as input: an MSA of myAb1 genes to train the model; an MSA of myAb1 genes whose color will be estimated; the encoder to use.

---
+++ WineCol.py
@@ -0,0 +1,20 @@
+import argparse
+import WineCol_model
+
+parser = argparse.ArgumentParser(description='Estimates color of grapes from genotype data using a support vector machine')
+parser.add_argument('train', type = str,
+                     help = 'Fasta file consisting of a multiple sequence alignment of the myAb1 genes used to train the model')
+parser.add_argument('test', type = str,
+                     help = 'Fasta file with the sequence/s of the myAb1 gene whose color will be estimated')
+
+parser.add_argument("-e", type=int, choices=[0, 1, 2],
+                    help="encoder to use. Default 0")
+args = parser.parse_args()
+train = args.train
+test = args.test
+if args.e:
+    encoder = str(args.e)
+else:
+    encoder = 0
+
+WineCol_model.predict_grape('SNP_GAPS_without_Gret1_400N.fa', 'test.fa', encoder)
